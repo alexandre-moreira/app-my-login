bd96dd1eab8a1aa6647a922187210198e2d59a23
Alexandre Moreira <4lexandre.moreira@gmail.com>
2023-11-13 17:29:43 -0300

    feat: acao formulario  LoginPage

--- src/pages/LoginPage/index.jsx
+++ src/pages/LoginPage/index.jsx
@@ -1,26 +1,46 @@
-import React from 'react'
+import React, { useState } from "react";
 
-import './styles.css';
+import "./styles.css";
 
 const LoginPage = () => {
+  const [email, setEmail] = useState("");
+  const [passowrd, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("submit", email, passowrd);
+  };
+
   return (
     <div id="login">
-        <h1 className="title">Login do sistema</h1>
-        <form className="form">
-            <div className="field">
-                <label htmlFor="email">Email</label>
-                <input type="email" name="email" id="email" />
-            </div>
-            <div className="field">
-                <label htmlFor="passowrd">Senha</label>
-                <input type="passowrd" name="passowrd" id="passowrd" />
-            </div>
-            <div className="actions">
-                <button type="submit">Entrar</button>
-            </div>
-        </form>
+      <h1 className="title">Login do sistema</h1>
+      <form className="form" onSubmit={handleSubmit}>
+        <div className="field">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className="field">
+          <label htmlFor="password">Senha</label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            value={passowrd}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <div className="actions">
+          <button type="submit">Entrar</button>
+        </div>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default LoginPage
+export default LoginPage;
